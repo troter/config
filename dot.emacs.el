682869326d2e67cd3cb4fcbd9cce826141a03731
イイノ タクミ <trot.thunder@gmail.com>
2009-02-04 00:54:32 +0900

    add yasnippet setting

--- dot.emacs.el
+++ dot.emacs.el
@@ -387,6 +387,16 @@
 			     (define-key term-raw-map "\C-n" 'next-line)
 			     (term-set-escape-char ?\C-x)))
 
+(require 'yasnippet)
+(require 'anything-c-yasnippet)
+(setq anything-c-yas-space-match-any-greedy t) ;スペース区切りで絞り込めるようにする デフォルトは nil
+(global-set-key (kbd "C-c y") 'anything-c-yas-complete) ;C-c yで起動 (同時にお使いのマイナーモードとキーバインドがかぶるかもしれません)
+(yas/initialize)
+(yas/load-directory "~/.emacs.d/elisp/yasnippet/snippets/") ;snippetsのディレクトリを指定
+(add-to-list 'yas/extra-mode-hooks 'ruby-mode-hook)
+(add-to-list 'yas/extra-mode-hooks 'cperl-mode-hook)
+
+
 
 ;;;============================================================
 ;;; programming major modes
