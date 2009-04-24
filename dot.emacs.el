32357e062c66cfb38bbd4d5a7b64d528af2d8307
イイノ タクミ <trot.thunder@gmail.com>
2009-04-25 02:07:36 +0900

    * move unicode settings.

--- dot.emacs.el
+++ dot.emacs.el
@@ -57,6 +57,28 @@
         (t
          (setup-language-environment-unix))))
 
+; http://www.pqrs.org/tekezo/emacs/doc/wide-character/index.html
+(utf-translate-cjk-set-unicode-range
+ '((#x00a2 . #x00a3)                    ; ¢, £
+   (#x00a7 . #x00a8)                    ; §, ¨
+   (#x00ac . #x00ac)                    ; ¬
+   (#x00b0 . #x00b1)                    ; °, ±
+   (#x00b4 . #x00b4)                    ; ´
+   (#x00b6 . #x00b6)                    ; ¶
+   (#x00d7 . #x00d7)                    ; ×
+   (#X00f7 . #x00f7)                    ; ÷
+   (#x0370 . #x03ff)                    ; Greek and Coptic
+   (#x0400 . #x04FF)                    ; Cyrillic
+   (#x2000 . #x206F)                    ; General Punctuation
+   (#x2100 . #x214F)                    ; Letterlike Symbols
+   (#x2190 . #x21FF)                    ; Arrows
+   (#x2200 . #x22FF)                    ; Mathematical Operators
+   (#x2300 . #x23FF)                    ; Miscellaneous Technical
+   (#x2500 . #x257F)                    ; Box Drawing
+   (#x25A0 . #x25FF)                    ; Geometric Shapes
+   (#x2600 . #x26FF)                    ; Miscellaneous Symbols
+   (#x2e80 . #xd7a3) (#xff00 . #xffef)))
+
 
 ;;; Input method settings.
 ;;; ======================
@@ -611,27 +633,6 @@
 ;      ;;"<li><a[^?]*\\?cmd=read&page=\\([^\"]*\\)\"[^>]*>\\([^<]*\\)</a>[<small>]*\\([^<]*\\)[</small>]*</li>"
 ;      "<li><a[^?]*\\?\\([^\"]*\\)\">\\([^<]*\\)</a>[<small>]*\\([^<]*\\)[</small>]*</li>")
 
-; http://www.pqrs.org/tekezo/emacs/doc/wide-character/index.html
-(utf-translate-cjk-set-unicode-range
- '((#x00a2 . #x00a3)                    ; ¢, £
-   (#x00a7 . #x00a8)                    ; §, ¨
-   (#x00ac . #x00ac)                    ; ¬
-   (#x00b0 . #x00b1)                    ; °, ±
-   (#x00b4 . #x00b4)                    ; ´
-   (#x00b6 . #x00b6)                    ; ¶
-   (#x00d7 . #x00d7)                    ; ×
-   (#X00f7 . #x00f7)                    ; ÷
-   (#x0370 . #x03ff)                    ; Greek and Coptic
-   (#x0400 . #x04FF)                    ; Cyrillic
-   (#x2000 . #x206F)                    ; General Punctuation
-   (#x2100 . #x214F)                    ; Letterlike Symbols
-   (#x2190 . #x21FF)                    ; Arrows
-   (#x2200 . #x22FF)                    ; Mathematical Operators
-   (#x2300 . #x23FF)                    ; Miscellaneous Technical
-   (#x2500 . #x257F)                    ; Box Drawing
-   (#x25A0 . #x25FF)                    ; Geometric Shapes
-   (#x2600 . #x26FF)                    ; Miscellaneous Symbols
-   (#x2e80 . #xd7a3) (#xff00 . #xffef)))
 
 
 ;;; End of .emacs.el
