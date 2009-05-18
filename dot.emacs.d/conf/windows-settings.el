5ca00fb5866898f7234debb2acfb68f1dfc70c58
takumi <trot.thunder@gmail.com>
2009-05-18 20:20:36 +0900

    use when. set cygwin-mount-cygwin-bin-directory

--- dot.emacs.d/conf/windows-settings.el
+++ dot.emacs.d/conf/windows-settings.el
@@ -1,20 +1,19 @@
 ;;; -*- coding: utf-8; indent-tabs-mode: nil -*-
 ;;; windows-setting.el ---
 
-(if (windows-ntp)
-    (progn
-
-      ;; argument-editing の設定
-      (require 'mw32script)
-      (mw32script-init)
-      (setq exec-suffix-list '(".exe" ".sh" ".pl"))
-      (setq shell-file-name-chars "~/A-Za-z0-9_^$!#%&{}@`'.:()-")
-
-      ;; cygwin-mount
-      ;; (install-elisp "http://www.emacswiki.org/cgi-bin/wiki/download/cygwin-mount.el")
-      (require 'cygwin-mount)
-      (cygwin-mount-activate)
-
-      ))
+(when (windows-ntp)
+  ;; argument-editing の設定
+  (when (require 'mw32script nil t)
+    (mw32script-init)
+    (setq exec-suffix-list '(".exe" ".sh" ".pl"))
+    (setq shell-file-name-chars "~/A-Za-z0-9_^$!#%&{}@`'.:()-"))
+  ;; cygwin-mount
+  ;; (install-elisp "http://www.emacswiki.org/cgi-bin/wiki/download/cygwin-mount.el")
+  (setq cygwin-mount-cygwin-bin-directory
+        (concat (getenv "windir") "\\..\\" "cygwin\\bin"))
+  (when (require 'cygwin-mount)
+    (cygwin-mount-activate)
+    )
+)
 
 ;;; windows-setting.el ends here
