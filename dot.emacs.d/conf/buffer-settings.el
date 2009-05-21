ed12602eb81afef296cf9e4505203a2f420cf764
takumi <trot.thunder@gmail.com>
2009-05-21 15:59:37 +0900

    add moccur

--- dot.emacs.d/conf/buffer-settings.el
+++ dot.emacs.d/conf/buffer-settings.el
@@ -1,54 +1,19 @@
 ;;; -*- coding: utf-8; indent-tabs-mode: nil -*-
 ;;; buffer-setting.el --- setup buffer utility
 
-;; protbuf
-;; (install-elisp "http://www.splode.com/~friedman/software/emacs-lisp/src/protbuf.el")
-(require 'protbuf) 
-
-;; Phenix Scratch Buffer
-;; URI: http://www.bookshelf.jp/soft/meadow_29.html#SEC381
-(defun my-make-scratch (&optional arg) 
-  (interactive)
-  (progn
-    ;; "*scratch*" を作成して buffer-list に放り込む
-    (set-buffer (get-buffer-create "*scratch*"))
-    (funcall initial-major-mode)
-    (erase-buffer)
-    (when (and initial-scratch-message (not inhibit-startup-message))
-      (insert initial-scratch-message))
-    (or arg (progn (setq arg 0)
-                   (switch-to-buffer "*scratch*")))
-    (cond ((= arg 0) (message "*scratch* is cleared up."))
-          ((= arg 1) (message "another *scratch* is created")))))
-
-(defun my-buffer-name-list ()
-  (mapcar (function buffer-name) (buffer-list)))
-
-(add-hook 'kill-buffer-query-functions
-          ;; *scratch* バッファーで kill-buffer したら内容を消去すだけにする
-          (function (lambda ()
-                      (if (string= "*scratch*" (buffer-name))
-                          (progn (my-make-scratch 0) nil)
-                        t))))
-(add-hook 'after-save-hook
-          ;; *scratch* バッファの内容を保存したら *scratch* バッファを新しく作る
-          (function (lambda ()
-                      (unless (member "*scratch*" (my-buffer-name-list))
-                        (my-make-scratch 1)))))
-
-;; Auto save buffers enhanced
-;; http://coderepos.org/share/browser/lang/elisp/auto-save-buffers-enhanced/trunk/auto-save-buffers-enhanced.el
-;; (require 'auto-save-buffers-enhanced)
-;; (auto-save-buffers-enhanced-include-only-checkout-path t)
-;; (auto-save-buffers-enhanced t)
+;; color-moccur
+;; (install-elisp-from-emacswiki "color-moccur.el")
+;; (install-elisp-from-emacswiki "moccur-edit.el")
+(require 'color-moccur)
+(require 'moccur-edit)
 
 ;; http://www.bookshelf.jp/soft/meadow_16.html#SEC155
-;; <N>というsuffixを付けない
+;; <Number>というsuffixを<Parent Directory>にする
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'post-forward-angle-brackets)
 (setq uniquify-ignore-buffers-re "*[^*]+*")
 
-;; Meadow/Emacs memo
+;; iswitch buffers
 (iswitchb-mode 1)
 (add-hook 'iswitchb-define-mode-map-hook 'iswitchb-my-keys)
 
@@ -95,5 +60,46 @@
                                    (expand-file-name dired-directory)))
                           (t
                            (concat "mode: " mode-name " Mode"))))))))
+
+;; protbuf
+;; (install-elisp "http://www.splode.com/~friedman/software/emacs-lisp/src/protbuf.el")
+;; (require 'protbuf) 
+
+;; Phenix Scratch Buffer
+;; URI: http://www.bookshelf.jp/soft/meadow_29.html#SEC381
+(defun my-make-scratch (&optional arg) 
+  (interactive)
+  (progn
+    ;; "*scratch*" を作成して buffer-list に放り込む
+    (set-buffer (get-buffer-create "*scratch*"))
+    (funcall initial-major-mode)
+    (erase-buffer)
+    (when (and initial-scratch-message (not inhibit-startup-message))
+      (insert initial-scratch-message))
+    (or arg (progn (setq arg 0)
+                   (switch-to-buffer "*scratch*")))
+    (cond ((= arg 0) (message "*scratch* is cleared up."))
+          ((= arg 1) (message "another *scratch* is created")))))
+
+(defun my-buffer-name-list ()
+  (mapcar (function buffer-name) (buffer-list)))
+
+(add-hook 'kill-buffer-query-functions
+          ;; *scratch* バッファーで kill-buffer したら内容を消去すだけにする
+          (function (lambda ()
+                      (if (string= "*scratch*" (buffer-name))
+                          (progn (my-make-scratch 0) nil)
+                        t))))
+(add-hook 'after-save-hook
+          ;; *scratch* バッファの内容を保存したら *scratch* バッファを新しく作る
+          (function (lambda ()
+                      (unless (member "*scratch*" (my-buffer-name-list))
+                        (my-make-scratch 1)))))
+
+;; Auto save buffers enhanced
+;; (install-elisp "http://coderepos.org/share/browser/lang/elisp/auto-save-buffers-enhanced/trunk/auto-save-buffers-enhanced.el" )
+;; (require 'auto-save-buffers-enhanced)
+;; (auto-save-buffers-enhanced-include-only-checkout-path t)
+;; (auto-save-buffers-enhanced t)
 
 ;;; buffer-setting.el ends here
